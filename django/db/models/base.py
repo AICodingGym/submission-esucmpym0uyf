4eaf11be56301665f928d07e4071aa34330e51ed
Arunima Anand <arunima.anand25@gmail.com>
2026-06-07 20:18:38 -0700

    Respect cleared parent primary keys on child save

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -804,7 +804,8 @@ class Model(metaclass=ModelBase):
         inserted = False
         for parent, field in meta.parents.items():
             # Make sure the link fields are synced between parent and self.
-            if (field and getattr(self, parent._meta.pk.attname) is None and
+            if (self._state.adding and field and
+                    getattr(self, parent._meta.pk.attname) is None and
                     getattr(self, field.attname) is not None):
                 setattr(self, parent._meta.pk.attname, getattr(self, field.attname))
             parent_inserted = self._save_parents(cls=parent, using=using, update_fields=update_fields)
